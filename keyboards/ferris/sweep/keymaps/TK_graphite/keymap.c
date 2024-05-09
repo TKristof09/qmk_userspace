f8310994bb7e4af7446150b1d153fbfe1529245c
Kristof Toth <toth.kristof111@gmail.com>
2024-05-09 20:19:50 +0200

    Layer hold for numbers

--- keyboards/ferris/sweep/keymaps/TK_graphite/keymap.c
+++ keyboards/ferris/sweep/keymaps/TK_graphite/keymap.c
@@ -489,11 +489,11 @@ void matrix_scan_user(void)
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[ALPHA_LAYER] = LAYOUT_split_3x5_2(
-            KC_Q,         KC_L,         KC_D,         KC_W,             KC_Z,          KC_SCLN,        KC_F,          KC_O,          KC_U,          KC_J,
-            MEH_T(KC_N),  LGUI_T(KC_R), LALT_T(KC_T), LCTL_T(KC_S),     KC_G,          KC_Y,    RCTL_T(KC_H),  LALT_T(KC_A),  RGUI_T(KC_E),   MEH_T(KC_I),
-            KC_B,         KC_X,         KC_M,         KC_C,             KC_V,          KC_K,           KC_P,     DOT_ARROW,       KC_COMM,        KC_MINS,
+            KC_Q,         KC_L,         KC_D,         KC_W,             KC_Z,                           KC_SCLN,        KC_F,          KC_O,          KC_U,          KC_J,
+            MEH_T(KC_N),  LGUI_T(KC_R), LALT_T(KC_T), LCTL_T(KC_S),     LT(NUM_LAYER, KC_G),            KC_Y,    RCTL_T(KC_H),  LALT_T(KC_A),  RGUI_T(KC_E),   MEH_T(KC_I),
+            KC_B,         KC_X,         KC_M,         KC_C,             KC_V,                           KC_K,           KC_P,     DOT_ARROW,       KC_COMM,        KC_MINS,
 
-                                                         OSM(MOD_LSFT), KC_BSPC,      NAV_HOLD,  SYM_WIN_LAYER),
+                                                         OSM(MOD_LSFT), KC_BSPC,                        NAV_HOLD,  SYM_WIN_LAYER),
 
 
 	[SYM_LAYER] = LAYOUT_split_3x5_2(
@@ -504,9 +504,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             TO(ALPHA_LAYER), KC_BSPC,        NAV_HOLD,   TO(ACCENT_LAYER)),
 
     [NUM_LAYER] = LAYOUT_split_3x5_2(
-            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_P7, KC_P8, KC_P9, KC_NO,
-            KC_PDOT, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS,    KC_P0,   KC_P4, KC_P5, KC_P6, KC_EQL,
-            KC_NO,   KC_NO,   KC_NO,   KC_COMM, KC_NO,      KC_NO,   KC_P1, KC_P2, KC_P3, KC_NO,
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_PPLS,   KC_P7, KC_P8, KC_P9, KC_NO,
+            KC_PDOT, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS,    KC_P0,     KC_P4, KC_P5, KC_P6, KC_EQL,
+            KC_NO,   KC_NO,   KC_COMM, KC_COMM, KC_NO,      KC_PMNS,   KC_P1, KC_P2, KC_P3, KC_NO,
 
                                TO(ALPHA_LAYER), KC_BSPC,    NAV_HOLD, TO(SYM_LAYER)),
 
